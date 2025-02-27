d1db2853bc5565fabcad60717c24285bb3fd3adc
ElianCoder <IanSantos@ElianVenturesLLC.onmicrosoft.com>
2025-02-27 13:49:15 -0800

    backend update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv/lib/main.js').config(); // Load environment variables
+require('dotenv').config();
 const fetch = require('isomorphic-fetch');
 const { Client } = require('@microsoft/microsoft-graph-client');
 
